eb5b6b3c7f55f6215dc0d82b9287f0a261020857
Nicholas Ng <nickng589@berkeley.edu>
2020-02-22 18:05:56 -0800

    player movement done

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -12,20 +12,31 @@ public class PlayerScript : MonoBehaviour
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        Vector3 stageDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        float width = GetComponent<SpriteRenderer>().bounds.size.x;
+        Debug.Log(stageDimensions.x);
+        Debug.Log(stageDimensions.y);
+        Debug.Log("width: " + width);
     }
 
     void Update()
     {
         xAxis = Input.GetAxisRaw("Horizontal");
-        yAxis = Input.GetAxisRaw("Vertical");
 
         move();
     }
 
     void move()
     {
-        Vector2 movementVector = new Vector2(xAxis, yAxis);
+        Vector2 movementVector = new Vector2(xAxis, 0);
         movementVector = movementVector * 4;
         playerRigidbody.velocity = movementVector;
+        if(xAxis < 0)
+        {
+            Debug.Log("LEFT");
+        } else if(xAxis > 0)
+        {
+            Debug.Log("RIGHT");
+        }
     }
 }
