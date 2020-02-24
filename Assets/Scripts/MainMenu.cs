0bef3324c0804bc6e97ac3c0e703724242628739
Sufyan Suliman <sufyan_suliman@berkeley.edu>
2020-02-23 21:24:30 -0800

    for UI

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    #region StartButton
+    private void Play()
+	{
+        SceneManageer.LoadScene("Cannon-Game");
+	}
+    #endregion
+
+    #region Reset HighScore
+    private void Reset()
+	{
+
+	}
+    #endregion
+}
+
