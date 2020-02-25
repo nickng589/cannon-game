033c311e7ec3004cb472af6fefd29383301dba15
Nicholas Ng <nickng589@berkeley.edu>
2020-02-24 18:48:04 -0800

    can break rocks and things

--- Assets/Scripts/RockScript.cs
+++ Assets/Scripts/RockScript.cs
@@ -16,6 +16,9 @@ public class RockScript : MonoBehaviour
     GameObject babyRock1;
 
     [SerializeField]
+    GameObject babyRock2;
+
+    [SerializeField]
     public int size;
 
     // Start is called before the first frame update
@@ -23,9 +26,11 @@ public class RockScript : MonoBehaviour
     {
         stageDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
         rockRigidbody = GetComponent<Rigidbody2D>();
-        rockRigidbody.velocity = new Vector2(Random.Range(-2.0f, 2.0f), 5);
+        rockRigidbody.velocity = new Vector2(Random.Range(-3.0f, 3.0f), Random.Range(5.0f,7.0f));
         number = (TextMesh) transform.GetChild(0).GetComponent(typeof(TextMesh));
         number.text = size.ToString();
+        babyRock1 = Resources.Load("Prefabs/RockCopy") as GameObject;
+        babyRock2 = Resources.Load("Prefabs/RockCopy") as GameObject;
     }
 
     // Update is called once per frame
@@ -42,13 +47,20 @@ public class RockScript : MonoBehaviour
         //Debug.Log("HI");
         if (coll.gameObject.tag == "Projectile")
         {
-            Debug.Log("HIT");
-            babyRock1.SetActive(false);
-            GameObject baby = Instantiate(babyRock1, transform.position, Quaternion.identity);
-            baby.GetComponent<RockScript>().size = 0;
-            baby.GetComponent<RockScript>().babyRock1 = babyRock1;
-            baby.SetActive(true);
-            //baby.GetComponent<RockScript>().rockRigidbody.velocity = new Vector2(Random.Range(-2.0f,1.0f), 5);
+            if (size > 1)
+            {
+                Debug.Log("HIT");
+                //babyRock1.SetActive(false);
+                GameObject baby = Instantiate(babyRock1, transform.position, Quaternion.identity);
+                baby.GetComponent<RockScript>().size = size/2;
+                //baby.GetComponent<RockScript>().babyRock1 = Resources.Load("Prefabs/RockCopy") as GameObject;
+                //baby.GetComponent<RockScript>().babyRock2 = Resources.Load("Prefabs/RockCopy") as GameObject;
+                GameObject baby2 = Instantiate(babyRock1, transform.position, Quaternion.identity);
+                baby2.GetComponent<RockScript>().size = size / 2;
+                //baby2.GetComponent<RockScript>().babyRock2 = Resources.Load("Prefabs/RockCopy") as GameObject;
+                //baby2.GetComponent<RockScript>().babyRock1 = Resources.Load("Prefabs/RockCopy") as GameObject;
+                //baby.SetActive(true);
+            }
             Destroy(gameObject);
         }
         if (coll.gameObject.tag == "Player")
