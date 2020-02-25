841f4c924e9e19adc06b4b4f2c7d2530e4ce731b
Nicholas Ng <nickng589@berkeley.edu>
2020-02-24 18:01:49 -0800

    added rock bouncing effect

--- Assets/Scripts/RockScript.cs
+++ Assets/Scripts/RockScript.cs
@@ -3,17 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RockScript : MonoBehaviour
+
 {
+
+    private Rigidbody2D rockRigidbody;
+
+    private Vector3 stageDimensions;
+
+    public TextMesh number;
+
+    [SerializeField]
+    GameObject babyRock1;
+
+    [SerializeField]
+    public int size;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        stageDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        rockRigidbody = GetComponent<Rigidbody2D>();
+        rockRigidbody.velocity = new Vector2(Random.Range(-2.0f, 2.0f), 5);
+        number = (TextMesh) transform.GetChild(0).GetComponent(typeof(TextMesh));
+        number.text = size.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.y < -stageDimensions.y)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D coll)
@@ -22,6 +43,12 @@ public class RockScript : MonoBehaviour
         if (coll.gameObject.tag == "Projectile")
         {
             Debug.Log("HIT");
+            babyRock1.SetActive(false);
+            GameObject baby = Instantiate(babyRock1, transform.position, Quaternion.identity);
+            baby.GetComponent<RockScript>().size = 0;
+            baby.GetComponent<RockScript>().babyRock1 = babyRock1;
+            baby.SetActive(true);
+            //baby.GetComponent<RockScript>().rockRigidbody.velocity = new Vector2(Random.Range(-2.0f,1.0f), 5);
             Destroy(gameObject);
         }
         if (coll.gameObject.tag == "Player")
