e95b3005d3573bdb663a88b04baf9df1bb935072
Sufyan Suliman <sufyan_suliman@berkeley.edu>
2020-02-24 18:46:27 -0800

    UI Done

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,10 +7,41 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private string m_Default;
+
+    [SerializeField]
+    private Text m_HighScore;
+
     public void Awake()
     {
-        return;
+        m_Default = m_HighScore.text;
     }
+
+    private void Start()
+    {
+        UpdateHS();
+    }
+
+    #region High Score
+    private void UpdateHS()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            m_HighScore.text = m_Default.Replace("$S", PlayerPrefs.GetInt("HighScore").ToString());
+        } else
+        {
+            PlayerPrefs.SetInt("HighScore", 0);
+            m_HighScore.text = m_Default.Replace("$S", "0");
+        }
+    }
+
+    public void Reset()
+    {
+        PlayerPrefs.SetInt("HighScore", 0);
+        UpdateHS();
+    }
+    #endregion
+
 
     #region StartButton
     public void Play()
