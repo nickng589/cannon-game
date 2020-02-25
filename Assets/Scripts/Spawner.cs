4bee635cc3d10ca2b9bcd868b503fa5fb689d383
Nicholas Ng <nickng589@berkeley.edu>
2020-02-24 20:04:39 -0800

    rocks destroyed

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,22 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
     [SerializeField]
     GameObject rock;
 
     private Vector3 stageDimensions;
 
+    [SerializeField]
+    int delay;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        stageDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+    }
+
+    void Awake()
+    {
+        StartCoroutine(Spawn());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(delay);
+        Instantiate(rock, new Vector3(Random.Range(stageDimensions.x, stageDimensions.x), stageDimensions.y), Quaternion.identity);
     }
 }
